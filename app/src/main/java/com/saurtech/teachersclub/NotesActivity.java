5a68f40f7380a8d650bb7f99b97c0663d317dacb
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-08-12 18:33:23 +0530

    Ads in Notes Activity

--- app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
+++ app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
@@ -1,9 +1,17 @@
 package com.saurtech.teachersclub;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
+import com.google.android.gms.ads.initialization.InitializationStatus;
+import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class NotesActivity extends AppCompatActivity {
 
@@ -19,5 +27,21 @@ public class NotesActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayShowTitleEnabled(false);
         }
+
+        //Add
+        MobileAds.initialize(this, new OnInitializationCompleteListener() {
+            @Override
+            public void onInitializationComplete(@NonNull InitializationStatus initializationStatus) {
+            }
+        });
+
+        AdView adView = new AdView(this);
+        adView.setAdSize(AdSize.BANNER);
+        adView.setAdUnitId("ca-app-pub-3940256099942544/6300978111");
+
+        adView = findViewById(R.id.adViewNotes);
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+        //Add
     }
 }
