e27089134c7ff5105340d9d17872007b103bef0f
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-08-12 18:17:25 +0530

    Notes Activity

--- app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
+++ app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
@@ -1,14 +1,23 @@
 package com.saurtech.teachersclub;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
 public class NotesActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notes);
+
+        toolbar = findViewById(R.id.notesToolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
