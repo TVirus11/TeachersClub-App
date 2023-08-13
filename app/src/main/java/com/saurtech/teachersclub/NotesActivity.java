5c5eaf68d3ddb06ee688babbe645903ba5f93d70
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-08-13 19:49:29 +0530

    Permissions

--- app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
+++ app/src/main/java/com/saurtech/teachersclub/NotesActivity.java
@@ -1,21 +1,18 @@
 package com.saurtech.teachersclub;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.cardview.widget.CardView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
-import com.google.android.gms.ads.initialization.InitializationStatus;
-import com.google.android.gms.ads.initialization.OnInitializationCompleteListener;
 
 public class NotesActivity extends AppCompatActivity {
 
