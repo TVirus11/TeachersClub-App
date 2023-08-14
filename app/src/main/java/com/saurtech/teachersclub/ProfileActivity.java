b86a7ed91f480ae9e04f2d7948c19953f77180d0
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-08-14 11:45:38 +0530

    Profile Done

--- app/src/main/java/com/saurtech/teachersclub/ProfileActivity.java
+++ app/src/main/java/com/saurtech/teachersclub/ProfileActivity.java
@@ -9,22 +9,9 @@ import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    LinearLayout profileBg;
-
-    ImageView profileBackArrow, profilePic;
-    TextView profileName, profileEmail;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
-        profileBg = findViewById(R.id.profile_bg);
-
-        profileBackArrow = findViewById(R.id.profile_back_arrow);
-        profilePic = findViewById(R.id.profile_pic);
-
-        profileName = findViewById(R.id.profile_name);
-        profileEmail = findViewById(R.id.profile_email);
     }
 }
