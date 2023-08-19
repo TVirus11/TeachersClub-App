7aff64c48a0ba09ac4520d5417967be4bf5407cb
DroidSaurav <131275580+DroidSaurav@users.noreply.github.com>
2023-08-19 06:54:02 +0530

    Community FAB

--- app/src/main/java/com/saurtech/teachersclub/CommunityActivity.java
+++ app/src/main/java/com/saurtech/teachersclub/CommunityActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdSize;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.MobileAds;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class CommunityActivity extends AppCompatActivity {
+
+    FloatingActionButton mCreatePost, mAskQuestion;
+    ExtendedFloatingActionButton mCreate;
+    TextView mCreatePostTV, mAskQuestionTV;
+    Boolean areAllFABsVisible;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,46 @@ public class CommunityActivity extends AppCompatActivity {
             startActivity(backIntent);
             finish();
         });
+
+        mCreate = findViewById(R.id.mainFAB);
+
+        mCreatePost = findViewById(R.id.create_post_FAB);
+        mAskQuestion = findViewById(R.id.ask_question_FAB);
+
+        mCreatePostTV = findViewById(R.id.create_post_TV);
+        mAskQuestionTV = findViewById(R.id.create_question_TV);
+
+        mCreatePost.setVisibility(View.GONE);
+        mAskQuestion.setVisibility(View.GONE);
+        mCreatePostTV.setVisibility(View.GONE);
+        mAskQuestionTV.setVisibility(View.GONE);
+
+        areAllFABsVisible = false;
+
+        mCreate.shrink();
+
+        mCreate.setOnClickListener(v -> {
+            if (!areAllFABsVisible) {
+                mCreatePost.show();
+                mAskQuestion.show();
+                mCreatePostTV.setVisibility(View.VISIBLE);
+                mAskQuestionTV.setVisibility(View.VISIBLE);
+
+                mCreate.extend();
+
+                areAllFABsVisible = true;
+            } else {
+                mCreatePost.hide();
+                mAskQuestion.hide();
+                mCreatePostTV.setVisibility(View.GONE);
+                mAskQuestionTV.setVisibility(View.GONE);
+
+                mCreate.shrink();
+
+                areAllFABsVisible = false;
+            }
+        });
+
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setSelectedItemId(R.id.bot_nav_home);
